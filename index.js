01e3e87e277a4be90e5c982f9a9b17e1d96eeafe
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-11-29 20:22:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB();
 const app = express()
 
 const corsOption={
-    origin:"http://localhost:3000/",
+    origin:"https://mernmovie.netlify.app",
     credentials: true
 }
 //cros connection
